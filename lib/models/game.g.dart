bd6eb2798c22f8edd9afcdf804e6c4cc04a63e23
MorningKnight <www.harshitrajput2003@gmail.com>
2025-09-21 11:42:38 +0530

    Feat: Set up Hive database and Game data model

---
+++ lib/models/game.g.dart
@@ -0,0 +1,53 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'game.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class GameAdapter extends TypeAdapter<Game> {
+  @override
+  final int typeId = 0;
+
+  @override
+  Game read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Game(
+      title: fields[0] as String,
+      platform: fields[1] as String,
+      genre: fields[2] as String,
+      status: fields[3] as String,
+      dateAdded: fields[4] as DateTime,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Game obj) {
+    writer
+      ..writeByte(5)
+      ..writeByte(0)
+      ..write(obj.title)
+      ..writeByte(1)
+      ..write(obj.platform)
+      ..writeByte(2)
+      ..write(obj.genre)
+      ..writeByte(3)
+      ..write(obj.status)
+      ..writeByte(4)
+      ..write(obj.dateAdded);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is GameAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
